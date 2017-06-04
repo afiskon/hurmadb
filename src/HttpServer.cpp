938faa65611ae8c3f502a76f2cf60451a53821b0
Alexey Chernyshov <Alexey.N.Chernyshov@gmail.com>
2017-06-04 23:40:12 +0300

    Fix select EINTR

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -305,8 +305,12 @@ void HttpServer::accept(const std::atomic_bool& terminate_flag) {
         if(res > 0) /* ready to accept() */
             break;
 
-        if(res < 0) /* error */
-            throw std::runtime_error("HttpServer::accept() - select() call failed");
+        if(res < 0) { /* error */
+            if(errno == EINTR)
+                continue;
+            throw std::runtime_error("HttpServer::accept() - select() call failed: " +
+                                     std::string(strerror(errno)));
+        }
 
         /* timeout - check a terminate flag */
         if(terminate_flag.load())

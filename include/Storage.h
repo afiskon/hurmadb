d221947273c052239801b4a64f4d3d45ee383691
NikitOS <zhuchkov94@gmail.com>
2017-10-13 16:48:16 +0300

    Little fix

--- include/Storage.h
+++ include/Storage.h
@@ -9,7 +9,7 @@ public:
     virtual void set(const std::string& key, const std::string& value, bool* append) = 0; 
     virtual std::string get(const std::string& key, bool* found) = 0;
     virtual std::string get(const std::string& key);
-    virtual std::string getRange(const std::string& key_from, const std::string& key_to);
+    virtual std::string getRange(const std::string& key_from, const std::string& key_to) = 0;
     virtual void del(const std::string& key, bool* found) = 0;
     virtual void del(const std::string& key);
 };

39d7063f5f2eedddbc06e7c7424fe52d9338ea58
nikita <zhuchkov94@gmail.com>
2017-12-26 02:27:53 +0300

    Pgsql server

---
+++ include/PgsqlErrorCodes.h
@@ -0,0 +1,9 @@
+/* vim: set ai et ts=4 sw=4: */
+
+#pragma once
+
+/*
+	This is not all error's codes of error messages in postgresql.
+	To see all error codes go to https://www.postgresql.org/docs/9.6/static/errcodes-appendix.html
+*/
+static const char warning_code[] = "01000";

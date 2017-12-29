aaf64ddee73f7b0b3286d7da6ce614b9ca4ad3e2
nikita <zhuchkov94@gmail.com>
2017-12-29 14:35:10 +0300

    Change httpserver_port from const to random

--- tests/PgSqlServerTest/PgSqlServerTest.py
+++ tests/PgSqlServerTest/PgSqlServerTest.py
@@ -14,18 +14,19 @@ import psycopg2
 
 START = os.environ.get('HURMADB_PORT') is None
 PORT = int(os.getenv('HURMADB_PORT', 8000 + int(random.random()*1000)))
-HTTPSERVER_PORT = 8080 #default port of http server
+HTTPSERVER_PORT = int(os.getenv('HURMADB_PORT', 7000 + int(random.random()*1000)))
 con = None
 cur = None
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 FLAG = '-p' #special flag for pgsql server port
+HTTP_FLAG = '-h' #special flag for http server port
 
-def hurmadb_start(port):
+def hurmadb_start(port, httpserver_port):
     """
     Start HurmaDB on given port, return an object that corresponds to the created process.
     """
     if START:
-        return subprocess.Popen(['../../hurmadb',FLAG, str(port)])
+        return subprocess.Popen(['../../hurmadb',FLAG, str(port), HTTP_FLAG, str(httpserver_port)])
     else:
         return None
 
@@ -50,7 +51,7 @@ def hurmadb_stop(pobj):
 class TestBasic:
     def setup_class(self):
         self.log = logging.getLogger('HurmaDB')
-        self.pobj = hurmadb_start(PORT)
+        self.pobj = hurmadb_start(PORT, HTTPSERVER_PORT)
         time.sleep(3) # Give RocksDB some time to initialize
         self.log.debug("HurmaDB started on port {}".format(PORT))
 
@@ -84,7 +85,7 @@ class TestBasic:
     def update_query_test(self):
         self.log.debug("Running update_query_test")
         cur.execute("UPDATE kv SET v = 'aaa' WHERE k = 'ccc'")
-        assert(1==cur.rowcount)
+        assert(2==cur.rowcount)
 
     def select_by_key_query_test(self):
         self.log.debug("Running update_query_test")

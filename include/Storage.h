d77d8e7b1ee9ed8988cff33ebd363307ad46a3f5
Aleksander Alekseev <afiskon@gmail.com>
2018-01-29 16:50:19 +0300

    The code was not compiling, oops!

--- include/Storage.h
+++ include/Storage.h
@@ -14,8 +14,6 @@ using namespace std;
  * Various storage implementations (persistent, in-memory, compressed, encrypted storages, etc)
  * should implement this interface.
  *
- * Storage is not copyable.
- *
  */
 class Storage {
 public:
@@ -27,7 +25,4 @@ public:
     virtual deque<vector<string>> getRange(const string& key_from, const string& key_to) = 0;
     virtual void del(const string& key, bool* found) = 0;
     virtual void del(const string& key);
-
-    Storage(Storage const&) = delete;
-    void operator=(Storage const&) = delete;
 };
